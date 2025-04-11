15b841648914d585b560cf552001f526245ae163
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2025-04-11 16:47:51 +0530

    Fix: TS issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,7 @@ import Chat from "./pages/Chat";
 function App() {
 
   return (
-    <>
     <Chat/>
-    </>
   )
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Fix for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
